0274d0f4489ab5ebc29851c37cb1fd650d975965
Wai Loon <wai.loon.theng@accenture.com>
2020-01-03 20:47:26 +0800

    Added materials for chapter 7 to 9

---
+++ e_commerce/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/wai_loon/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/wai_loon/AndroidStudioProjects/flutter_practices/chapter_7-8-9/e_commerce"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/wai_loon/sdk/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
