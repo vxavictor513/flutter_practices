7c4dfffb3663a636edf87b24515f10fc566e9183
Wai Loon <wai.loon.theng@accenture.com>
2020-01-03 20:45:17 +0800

    Added weather_app for learning layout, gestures, and animations

---
+++ weather_app/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:weather_app/models/src/app_settings.dart';
+import 'package:weather_app/page/page_container.dart';
+import 'package:weather_app/styles.dart';
+
+void main() {
+  var settings = AppSettings(); // Mocking user preferences
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Don't allow landscape mode
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) => runApp(MyApp(settings: settings)));
+}
+
+class MyApp extends StatelessWidget {
+  final AppSettings settings;
+
+  const MyApp({Key key, this.settings}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = ThemeData(
+      fontFamily: 'Cabin',
+      primaryColor: AppColor.midnightSky,
+      accentColor: AppColor.midnightCloud,
+      primaryTextTheme: Theme.of(context).textTheme.apply(
+            bodyColor: AppColor.textColorDark,
+            displayColor: AppColor.textColorDark,
+          ),
+      textTheme: Theme.of(context).textTheme.apply(
+            bodyColor: AppColor.textColorDark,
+            displayColor: AppColor.textColorDark,
+          ),
+    );
+
+    return MaterialApp(
+      title: 'Weather App',
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: PageContainer(settings: settings),
+    );
+  }
+}
